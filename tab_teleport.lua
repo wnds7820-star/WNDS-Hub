2e7f36717cc80e59a0c4d85e95398ac9fc7143e7
DELY <wnds7820@gmail.com>
2026-03-29 05:58:32 +0700

    Delete tab_teleport.lua

--- tab_teleport.lua
+++
@@ -1,6 +0,0 @@
-local Tabs = _G.WNDS_UI.Tabs
-Tabs.Teleport:Section({ Title = "Quick TP" })
-local tp = {"Spawn", "Shop", "Farm Area", "PVP Arena"}
-for _, v in pairs(tp) do
-    Tabs.Teleport:Button({ Title = "TP to " .. v, Callback = function() print("Teleporting...") end })
-end
