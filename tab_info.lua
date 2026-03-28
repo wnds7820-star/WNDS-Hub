c07f53f05b0e2cf766db24ac83f7231f11d459db
DELY <wnds7820@gmail.com>
2026-03-29 05:59:09 +0700

    Delete tab_info.lua

--- tab_info.lua
+++
@@ -1,8 +0,0 @@
-local Tabs = _G.WNDS_UI.Tabs
-Tabs.Info:Section({ Title = "User Dashboard" })
-Tabs.Info:Paragraph({
-    Title = "Welcome, " .. game.Players.LocalPlayer.DisplayName,
-    Desc = "• Account ID: " .. game.Players.LocalPlayer.UserId .. "\n• Version: 5.4 Premium\n• Status: Active",
-    Image = "rbxthumb://type=AvatarHeadShot&id=" .. game.Players.LocalPlayer.UserId .. "&w=150&h=150",
-    ImageSize = 64
-})
