a64460904eec20d8a69c6c4e2e5feadbacc210f5
DELY <wnds7820@gmail.com>
2026-03-28 19:55:06 +0700

    Create tab_teleport.lua

---
+++ tab_teleport.lua
@@ -0,0 +1,7 @@
+local Tabs = _G.WNDS_UI.Tabs
+Tabs.Teleport:Section({ Title = "World Teleport" })
+Tabs.Teleport:Button({ Title = "TP to Spawn", Callback = function() end })
+Tabs.Teleport:Button({ Title = "TP to Random Player", Callback = function() end })
+Tabs.Teleport:Button({ Title = "Click TP", Callback = function() end })
+Tabs.Teleport:Button({ Title = "Save Current Position", Callback = function() end })
+Tabs.Teleport:Button({ Title = "Load Saved Position", Callback = function() end })
