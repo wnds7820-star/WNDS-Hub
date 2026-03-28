e5105e1690b860c93a15ef2b5856e47bce1a7ac4
DELY <wnds7820@gmail.com>
2026-03-28 19:29:15 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -14,6 +14,6 @@ _G.WNDS_Data = loadstring(game:HttpGet(base_url .. "https://github.com/wnds7820-
 _G.WNDS_UI = loadstring(game:HttpGet(base_url .. "ui_config.lua"))()
 
 -- 4. Load Core Functions (Combat, Visual, dll)
-loadstring(game:HttpGet(base_url .. "https://github.com/wnds7820-star/WNDS-Hub/blob/main/ui_config.lua"))()
+loadstring(game:HttpGet(base_url .. "https://github.com/wnds7820-star/WNDS-Hub/blob/main/functions.lua"))()
 
 print("WNDS Hub: All Modules Loaded Successfully!")
