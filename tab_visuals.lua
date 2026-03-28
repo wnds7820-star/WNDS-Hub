64ea121764456e8947d4ed980e737282c844f95e
DELY <wnds7820@gmail.com>
2026-03-29 05:58:13 +0700

    Delete tab_visuals.lua

--- tab_visuals.lua
+++
@@ -1,8 +0,0 @@
-local Tabs = _G.WNDS_UI.Tabs
-Tabs.Visuals:Section({ Title = "ESP" })
-local vists = {"Player ESP", "Box ESP", "Tracer ESP", "Name ESP", "Chams (Wallhack)"}
-for _, v in pairs(vists) do
-    Tabs.Visuals:Toggle({ Title = v, Default = false, Callback = function(s) print(v .. " is " .. tostring(s)) end })
-end
-Tabs.Visuals:Section({ Title = "World" })
-Tabs.Visuals:Toggle({ Title = "Full Bright", Default = false, Callback = function(v) game.Lighting.Brightness = v and 2 or 1 end })
