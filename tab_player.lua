ac8066c6ecf09aa2691f1e492663c4f07115965b
DELY <wnds7820@gmail.com>
2026-03-28 23:15:01 +0700

    Update tab_player.lua

--- tab_player.lua
+++ tab_player.lua
@@ -1,10 +1,10 @@
 local Tabs = _G.WNDS_UI.Tabs
-Tabs.Player:Section({ Title = "Physical" })
-
-Tabs.Player:Slider({ Title = "WalkSpeed", Min = 16, Max = 500, Default = 16, Callback = function(v) game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v end })
-Tabs.Player:Slider({ Title = "JumpPower", Min = 50, Max = 500, Default = 50, Callback = function(v) game.Players.LocalPlayer.Character.Humanoid.JumpPower = v end })
-
-local Toggles = {"Infinite Jump", "Fly Mode", "No Clip", "God Mode", "Anti-Fling", "Auto Farm", "Spin Bot", "Invisibility"}
-for _, v in pairs(Toggles) do
-    Tabs.Player:Toggle({ Title = v, Default = false, Callback = function(val) print(v .. " is " .. tostring(val)) end })
+Tabs.Player:Section({ Title = "Movement" })
+Tabs.Player:Slider({ Title = "Speed", Min = 16, Max = 500, Default = 16, Callback = function(v) game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v end })
+Tabs.Player:Toggle({ Title = "Infinite Jump", Default = false, Callback = function(s) _G.InfJump = s end })
+Tabs.Player:Toggle({ Title = "Fly Mode", Default = false, Callback = function(s) _G.Fly = s end })
+Tabs.Player:Section({ Title = "Exploits" })
+local p = {"Noclip", "God Mode", "Anti-Fling", "Spin Bot", "Auto Farm"}
+for _, v in pairs(p) do
+    Tabs.Player:Toggle({ Title = v, Default = false, Callback = function(s) print(v .. " is " .. tostring(s)) end })
 end
