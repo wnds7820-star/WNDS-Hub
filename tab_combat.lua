2daefea1e108804cb62bc9732bbfb2f2fc27e0ea
DELY <wnds7820@gmail.com>
2026-03-29 05:59:19 +0700

    Delete tab_combat.lua

--- tab_combat.lua
+++
@@ -1,9 +0,0 @@
-local Tabs = _G.WNDS_UI.Tabs
-Tabs.Combat:Section({ Title = "Aim Assistance" })
-Tabs.Combat:Toggle({ Title = "Aimbot", Default = false, Callback = function(s) _G.Aimbot = s end })
-Tabs.Combat:Toggle({ Title = "Silent Aim", Default = false, Callback = function(s) _G.SilentAim = s end })
-Tabs.Combat:Section({ Title = "Gun Mods" })
-local guns = {"No Recoil", "No Spread", "Infinite Ammo", "Rapid Fire", "Kill Aura"}
-for _, v in pairs(guns) do
-    Tabs.Combat:Toggle({ Title = v, Default = false, Callback = function(s) print(v .. " is " .. tostring(s)) end })
-end
