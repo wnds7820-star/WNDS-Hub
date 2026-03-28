361505bf58465f2f0d484324bcf33296532dd540
DELY <wnds7820@gmail.com>
2026-03-28 23:15:47 +0700

    Update tab_visuals.lua

--- tab_visuals.lua
+++ tab_visuals.lua
@@ -1,7 +1,8 @@
 local Tabs = _G.WNDS_UI.Tabs
-Tabs.Visuals:Section({ Title = "ESP & World" })
-
-local Visuals = {"Player ESP", "Box ESP", "Tracer ESP", "Skeleton ESP", "Distance ESP", "Item ESP", "Full Bright", "No Fog", "Wallhack", "Chams"}
-for _, v in pairs(Visuals) do
-    Tabs.Visuals:Toggle({ Title = v, Default = false, Callback = function(v) print(v) end })
+Tabs.Visuals:Section({ Title = "ESP" })
+local vists = {"Player ESP", "Box ESP", "Tracer ESP", "Name ESP", "Chams (Wallhack)"}
+for _, v in pairs(vists) do
+    Tabs.Visuals:Toggle({ Title = v, Default = false, Callback = function(s) print(v .. " is " .. tostring(s)) end })
 end
+Tabs.Visuals:Section({ Title = "World" })
+Tabs.Visuals:Toggle({ Title = "Full Bright", Default = false, Callback = function(v) game.Lighting.Brightness = v and 2 or 1 end })
