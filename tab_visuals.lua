9ab6185bedb790a4b9de58b2b2590cbdbeb85470
DELY <wnds7820@gmail.com>
2026-03-28 22:31:56 +0700

    Update tab_visuals.lua

--- tab_visuals.lua
+++ tab_visuals.lua
@@ -1,12 +1,7 @@
-local Tabs = _G.WNDS_UI.Tabs -- WAJIB ADA DI PALING ATAS
+local Tabs = _G.WNDS_UI.Tabs
+Tabs.Visuals:Section({ Title = "ESP & World" })
 
-Tabs.Visuals:Section({ Title = "ESP & Rendering" })
-
-Tabs.Visuals:Toggle({
-    Title = "Enable ESP",
-    Default = false,
-    Callback = function(Value)
-        print("ESP is: ", Value)
-        -- Masukkan script ESP kamu di sini
-    end
-})
+local Visuals = {"Player ESP", "Box ESP", "Tracer ESP", "Skeleton ESP", "Distance ESP", "Item ESP", "Full Bright", "No Fog", "Wallhack", "Chams"}
+for _, v in pairs(Visuals) do
+    Tabs.Visuals:Toggle({ Title = v, Default = false, Callback = function(v) print(v) end })
+end
