dd84063c57a412e280b31593330f1834916ddf2d
DELY <wnds7820@gmail.com>
2026-03-29 05:58:45 +0700

    Delete tab_settings.lua

--- tab_settings.lua
+++
@@ -1,7 +0,0 @@
-local Tabs = _G.WNDS_UI.Tabs
-Tabs.Settings:Section({ Title = "System" })
-Tabs.Settings:Button({ Title = "Reload Script", Callback = function() _G.WNDS_UI.Window:Destroy(); loadstring(game:HttpGet("https://raw.githubusercontent.com/wnds7820-star/WNDS-Hub/main/main.lua"))() end })
-local s = {"Anti-AFK", "Auto Rejoin", "Server Hop", "Low Graphics", "Show FPS", "Anti-Kick"}
-for _, v in pairs(s) do
-    Tabs.Settings:Toggle({ Title = v, Default = false, Callback = function(state) print(v .. " is " .. tostring(state)) end })
-end
