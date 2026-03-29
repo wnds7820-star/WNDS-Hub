38a1d7b73e43dd6c72175bbf786e55f0a2fdb485
DELY <wnds7820@gmail.com>
2026-03-29 21:03:22 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -65,11 +65,7 @@ end
 LoadModule("tab_home.lua")
 LoadModule("tab_player.lua")
 LoadModule("tab_combat.lua")
-LoadModule("tab_visual.lua")
-LoadModule("tab_world.lua")    
-LoadModule("tab_misc.lua")     
-LoadModule("tab_updates.lua")  
-LoadModule("tab_settings.lua") 
+LoadModule("tab_visual.lua") 
 
 -- Menghubungkan ke Tab pertama (Home) saat startup
 Window:SelectTab(1)
