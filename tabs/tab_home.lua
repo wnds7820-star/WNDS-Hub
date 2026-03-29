f7059aa732a9cc19fb51eb3b66703ae069b62c1c
DELY <wnds7820@gmail.com>
2026-03-29 17:52:46 +0700

    Update tab_home.lua

--- tabs/tab_home.lua
+++ tabs/tab_home.lua
@@ -1,8 +1,13 @@
 local HomeTab = _G.WNDS_Window:AddTab({ Title = "Home", Icon = "home" })
-local info = HomeTab:AddParagraph({ Title = "WNDS Statistics", Content = "Calculating..." })
 
+HomeTab:AddParagraph({ 
+    Title = "WNDS HUB v6.6 UPDATED!", 
+    Content = "- Added Smooth Aimbot (Lerp System)\n- Added Smooth Speed Transition\n- Improved ESP Stability" 
+})
+
+local info = HomeTab:AddParagraph({ Title = "Statistics", Content = "Loading..." })
 game:GetService("RunService").RenderStepped:Connect(function()
     local fps = math.floor(1/game:GetService("RunService").RenderStepped:Wait())
     local ping = math.floor(game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValue())
-    info:SetContent("FPS: " .. fps .. " | Ping: " .. ping .. "ms\nAccount: " .. game.Players.LocalPlayer.Name)
+    info:SetContent("FPS: " .. fps .. " | Ping: " .. ping .. "ms")
 end)
